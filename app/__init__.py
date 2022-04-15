9da33571d1be7f3675e8ce3a6f264d75f27180bf
Arthur Holanda <holandaarthur46@gmail.com>
2022-04-15 15:03:05 -0300

    Criação das migrations.

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from flask_migrate import Migrate, MigrateCommand
 app = Flask(__name__)
 manager = Manager(app)
 db = SQLAlchemy(app)
-migrate = Migrate(db, app)
+migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
 from app.controllers import default
