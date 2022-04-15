fece59f7d551afb035557bf38eb984cd1c829407
Arthur Holanda <holandaarthur46@gmail.com>
2022-04-15 19:19:48 -0300

    Melhorias

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-from pickle import TRUE
-
-
 DEBUG = True
 SECRET_KEY = "senha-forte"
-SQLALCHEMY_DATABASE_URI = 'mysql://root:20181174010034@localhost/storage'
+SQLALCHEMY_DATABASE_URI = "mysql://root:20181174010034@localhost/storage"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
