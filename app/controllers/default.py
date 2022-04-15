cf9106c92381933bf2d122a2e62153e6811efc6c
Arthur Holanda <holandaarthur46@gmail.com>
2022-04-15 14:58:27 -0300

    Inserindo a página principal.

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,7 @@
 from app import app
-
+from flask import render_template, request
 
 @app.route("/")
-def hello_word():
-    return 'Hello, Word!'
+@app.route("/index")
+def index():
+    return render_template('index.html')
--- config.py
+++ config.py
@@ -3,3 +3,5 @@ from pickle import TRUE
 
 DEBUG = True
 SECRET_KEY = "senha-forte"
+SQLALCHEMY_DATABASE_URI = 'mysql://root:20181174010034@localhost/storage'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
