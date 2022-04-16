12b6ab298c415c69777f87fb0249773acbe2ff5c
Arthur Holanda <holandaarthur46@gmail.com>
2022-04-15 21:26:58 -0300

    Melhorando Index

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,7 +1,14 @@
 from app import app
+from app.models.tables import Herois
 from flask import render_template, request
+from app import mysql
+import MySQLdb.cursors 
+
 
 @app.route("/")
-@app.route("/index")
+@app.route("/index", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute('SELECT * FROM herois;')
+    result = cur.fetchall()
+    return render_template("index.html", res=result)
