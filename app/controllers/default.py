775d0cb86371dc72512c0d2f97a14d4e2287fb9e
Arthur Holanda <holandaarthur46@gmail.com>
2022-04-16 13:11:53 -0300

    Função de Cadastro
    Essa função tanto cadastra um novo herói, quanto verifica se ele já está cadastrado na tabela.

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,14 +1,26 @@
 from app import app
-from app.models.tables import Herois
+from app.models.tables import Candidatos, Herois
 from flask import render_template, request
-from app import mysql
+from app import mysql, db
 import MySQLdb.cursors 
 
 
-@app.route("/")
-@app.route("/index", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/index/", methods=['GET', 'POST'])
 def index():
+    msg = ''
+    if request.method == 'POST':
+        x = request.form.getlist('mycheckbox')
+        for c in x:
+            presente = Candidatos.query.filter_by(nome=c).first()
+            if presente:
+                msg = 'Esse herói já foi cadastrado'
+            else:
+                me = Candidatos(c)
+                db.session.add(me)
+                db.session.commit()
+                msg = 'Sucesso'
     cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     cur.execute('SELECT * FROM herois;')
     result = cur.fetchall()
-    return render_template("index.html", res=result)
+    return render_template("index.html", res=result, mensagem=msg)
