3a3ba6252459f321cc821ad8521bd71349b04634
Arthur Holanda <holandaarthur46@gmail.com>
2022-04-15 21:27:17 -0300

    Melhorias no código

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,17 @@ from flask import Flask
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+from flask_mysqldb import MySQL
 
 
 
 app = Flask(__name__)
+mysql = MySQL(app)
 manager = Manager(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 app.config.from_object('config')
-
 from app.controllers import default
 
 
