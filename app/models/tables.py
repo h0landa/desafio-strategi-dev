1155fe3f1cb5f1a630adcd3d63bb9a7ab009caa9
Arthur Holanda <holandaarthur46@gmail.com>
2022-04-16 13:12:50 -0300

    Reformulando a tabela candidatos
    Reformulei a tabela candidato, de modo que ela fique apenas com o resistro dos nomes do heróis.

--- app/models/tables.py
+++ app/models/tables.py
@@ -25,12 +25,6 @@ class Candidatos(db.Model):
 
     id = db.Column(db.Integer, autoincrement = True, primary_key = True)
     nome = db.Column(db.String(45), unique = True)
-    caracteristicas = db.Column(db.String(100))
-    habilidades = db.Column(db.String(100))
-    poderes = db.Column(db.String(100))
 
-    def __init__(self, nome, caracteristicas, habilidades, poderes):
+    def __init__(self, nome):
         self.nome = nome
-        self.caracteristicas = caracteristicas
-        self.habilidades = habilidades
-        self.poderes = poderes
