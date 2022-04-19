ce3a5a3847ae78dde8c8d285260f378084a2178a
Arthur Holanda <holandaarthur46@gmail.com>
2022-04-19 08:19:23 -0300

    Criação do `config-example.py`

---
+++ config-example.py
@@ -0,0 +1,10 @@
+DEBUG = True
+SECRET_KEY = ''
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+DB = ''
+USER = 'root'
+PASSWORD = ''
+HOST = 'localhost'
+PORT = 3306
+SQLALCHEMY_DATABASE_URI = f'mysql://{USER}:{PASSWORD}@{HOST}/{DB}'
+
